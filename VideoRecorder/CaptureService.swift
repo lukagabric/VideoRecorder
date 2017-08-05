343a7651237d138dc3df859589e5b2dc6f2f8d67
Luka Gabric <luka.gabric@gmail.com>
2017-08-06 00:25:09 +0200

    Rec and preview

---
+++ VideoRecorder/CaptureService.swift
@@ -0,0 +1,130 @@
+//
+//  CaptureService.swift
+//  VideoRecorder
+//
+//  Created by Luka Gabric on 05/08/2017.
+//  Copyright © 2017 Luka Gabric. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+public class CaptureService: NSObject, AVCaptureFileOutputRecordingDelegate {
+	
+	//MARK: - Vars
+	
+	public let previewLayer: AVCaptureVideoPreviewLayer
+	private(set) public var isRecording: Bool
+	
+	private let session: AVCaptureSession
+
+	private let position: AVCaptureDevice.Position
+	private let quality: AVCaptureSession.Preset
+	
+	private var videoOutput: AVCaptureMovieFileOutput?
+	
+	//MARK: - Init
+	
+	init(position: AVCaptureDevice.Position) {
+		self.session = AVCaptureSession()
+		self.position = position
+		self.quality = .hd1280x720
+		self.previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+		self.isRecording = false
+		
+		super.init()
+		
+		self.configure()
+	}
+	
+	//MARK: - Configuration
+	
+	private func configure() {
+		self.session.sessionPreset = self.quality
+
+		self.configureVideoInput()
+		self.configureAudioInput()
+		self.configureOutput()
+	}
+	
+	private func configureVideoInput() {
+		guard let videoDevice = self.getVideoCaptureDevice() else { return }
+		self.configureInput(captureDevice: videoDevice)
+	}
+	
+	private func configureAudioInput() {
+		guard let audioDevice = self.getAudioCaptureDevice() else { return }
+		self.configureInput(captureDevice: audioDevice)
+	}
+	
+	private func getVideoCaptureDevice() -> AVCaptureDevice? {
+		let session = AVCaptureDevice.DiscoverySession(__deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera, .builtInTelephotoCamera],
+		                                               mediaType: .video,
+		                                               position: self.position)
+		return session.devices.filter { $0.hasMediaType(.video) && $0.position == self.position }.first
+	}
+	
+	private func getAudioCaptureDevice() -> AVCaptureDevice? {
+		let session = AVCaptureDevice.DiscoverySession(__deviceTypes: [.builtInMicrophone],
+		                                               mediaType: .audio,
+		                                               position: .unspecified)
+		return session.devices.filter { $0.hasMediaType(.audio) }.first
+	}
+	
+	private func configureInput(captureDevice: AVCaptureDevice) {
+		guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice),
+			self.session.canAddInput(captureDeviceInput) else { return }
+		self.session.addInput(captureDeviceInput)
+	}
+	
+	private func configureOutput() {
+		let videoOutput = AVCaptureMovieFileOutput()
+		
+		guard self.session.canAddOutput(videoOutput) else { return }
+		self.session.addOutput(videoOutput)
+		
+		guard let connection = videoOutput.connection(with: AVFoundation.AVMediaType.video),
+			connection.isVideoOrientationSupported,
+			connection.isVideoMirroringSupported else { return }
+		
+		connection.videoOrientation = .portrait
+		connection.isVideoMirrored = self.position == .front
+		
+		self.videoOutput = videoOutput
+	}
+	
+	//MARK: - Record
+	
+	public func startRecording(fileURL: URL) {
+		guard self.isRecording == false,
+			let videoOutput = self.videoOutput else { return }
+		
+		self.isRecording = true
+		
+		try? FileManager.default.removeItem(at: fileURL)
+		
+		self.session.startRunning()
+		
+		videoOutput.startRecording(to: fileURL, recordingDelegate: self)
+	}
+	
+	public func stopRecording() {
+		guard self.isRecording == true else { return }
+		
+		if let videoOutput = self.videoOutput {
+			videoOutput.stopRecording()
+		}
+		self.session.stopRunning()
+	}
+		
+	//MARK: - Delegate
+
+	public func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+		self.isRecording = false
+		print("did finish")
+	}
+	
+	//MARK: -
+
+}
--- VideoRecorder/ViewController.swift
+++ VideoRecorder/ViewController.swift
@@ -7,19 +7,150 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
-
+	
+	//MARK: - Vars
+	
+	private let captureService: CaptureService
+	private let previewLayer: AVCaptureVideoPreviewLayer
+	
+	@IBOutlet private weak var playButton: UIButton!
+	@IBOutlet private weak var recordButton: UIButton!
+	@IBOutlet private weak var previewContainer: UIView!
+	
+	private var player: AVPlayer?
+	private var playerLayer: AVPlayerLayer?
+	
+	//MARK: - Init
+	
+	init() {
+		self.captureService = CaptureService(position: .front)
+		self.previewLayer = self.captureService.previewLayer
+		self.previewLayer.videoGravity = .resizeAspectFill
+		super.init(nibName: "ViewController", bundle: nil)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	//MARK: - View Lifecycle
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		self.setButtonsEnabled(false)
+		
+		self.previewContainer.layer.addSublayer(self.previewLayer)
+		
+		self.checkPermissionAndRecord()
+	}
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		self.previewLayer.frame = self.previewContainer.bounds
+	}
+	
+	//MARK: - Permissions
+	
+	private func checkPermissionAndRecord() {
+		switch AVCaptureDevice.authorizationStatus(for: .video) {
+		case .authorized:
+			self.checkAudioPermissionAndRecord()
+		case .notDetermined:
+			self.requestVideoPermission()
+		default: break
+		}
+	}
+	
+	private func requestVideoPermission() {
+		AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+			guard let sself = self, granted == true else { return }
+			DispatchQueue.main.async {
+				sself.checkAudioPermissionAndRecord()
+			}
+		}
+	}
+	
+	private func checkAudioPermissionAndRecord() {
+		switch AVCaptureDevice.authorizationStatus(for: .audio) {
+		case .authorized:
+			self.setButtonsEnabled(true)
+		case .notDetermined:
+			self.requestAudioPermission()
+		default: break
+		}
+	}
+	
+	private func requestAudioPermission() {
+		AVCaptureDevice.requestAccess(for: .audio) { [weak self] granted in
+			guard let sself = self, granted == true else { return }
+			DispatchQueue.main.async {
+				sself.setButtonsEnabled(true)
+			}
+		}
+	}
+	
+	private func setButtonsEnabled(_ enabled: Bool) {
+		self.recordButton.isEnabled = enabled
+		self.playButton.isEnabled = enabled
+	}
+	
+	//MARK: - User Interaction
+	
+	@IBAction func recordAction(_ sender: Any) {
+		self.freePreview()
+		self.setButtonsEnabled(false)
+
+		self.captureService.startRecording(fileURL: self.createFileUrl())
+		DispatchQueue.main.asyncAfter(wallDeadline: .now() + 3) {
+			self.captureService.stopRecording()
+			
+			self.setButtonsEnabled(true)
+		}
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	@IBAction func playAction(_ sender: Any) {
+		self.freePreview()
+		
+		let videoURL = self.createFileUrl()
+
+		let player = AVPlayer(url: videoURL)
+		let playerLayer = AVPlayerLayer(player: player)
+		
+		self.player = player
+		self.playerLayer = playerLayer
+		
+		playerLayer.frame = self.previewContainer.bounds
+		playerLayer.videoGravity = .resizeAspectFill
+		
+		self.previewContainer.layer.addSublayer(playerLayer)
+		
+		player.play()
 	}
-
-
+	
+	@objc func playerDidFinishPlaying() {
+		self.freePreview()
+	}
+	
+	private func freePreview() {
+		self.player?.pause()
+		self.playerLayer?.removeFromSuperlayer()
+		self.playerLayer = nil
+		self.player = nil
+	}
+	
+	private func createFileUrl() -> URL {
+		let documentsUrl = FileManager.default.urls(for: .cachesDirectory, in:.userDomainMask).first!
+		let videoUrl = documentsUrl.appendingPathComponent("video12.mov")
+		return videoUrl
+	}
+	
+	//MARK: -
+	
 }
-
